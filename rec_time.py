7348dd6614f79db753a68dd98d1a2131a4e684ac
aaruff <anwarruff@gmail.com>
2018-01-12 18:01:57 -0500

    renamed subject_id to subject

--- rec_time.py
+++ rec_time.py
@@ -5,25 +5,25 @@ from sys import argv
 from os import remove
 from os.path import dirname, abspath, join
 
-def get_file_path(subject_id):
-    file_name = "timing_subject_{}.csv".format(subject_id)
+def get_file_path(subject):
+    file_name = "timing_subject_{}.csv".format(subject)
     path = join(join(dirname(dirname(abspath(__file__))), 'data'), file_name)
     return path
 
-def remove_file(subject_id):
-    file = get_file_path(subject_id)
+def remove_file(subject):
+    file = get_file_path(subject)
     try:
         print(file)
         remove(file)
     except OSError:
         pass
 
-def header(subject_id):
-    with open(get_file_path(subject_id), 'a') as f:
+def header(subject):
+    with open(get_file_path(subject), 'a') as f:
         f.write("subject, state, phase, time\n")
 
 def log(subject, state, phase, time):
-    with open(get_file_path(subject_id), 'a') as f:
+    with open(get_file_path(subject), 'a') as f:
         row_format = "{}, {}, {}, {}\n"
         line = row_format.format(subject, state, phase, time)
         f.write(line)
